f5b172071bc8f696d306dcaaf1d7e695589d3f92
Ben Puderer <bpuderer@yahoo.com>
2017-07-06 20:56:10 -0400

    specs

--- src/test/java/testlibs/RestAssuredWithWiremock.java
+++ src/test/java/testlibs/RestAssuredWithWiremock.java
@@ -4,6 +4,11 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
 import static io.restassured.RestAssured.given;
 import org.junit.*;
 import static org.hamcrest.Matchers.equalTo;
@@ -50,20 +55,27 @@ public class RestAssuredWithWiremock {
 
 	@Test
 	public void testExampleStatusCodeResponse() {
+		// can use specs when tests assert same response or use same request data
+		// move init into @BeforeClass
+		ResponseSpecification spec = new ResponseSpecBuilder().expectStatusCode(200)
+				.expectBody("value", equalTo(0)).build();
+		
 		given().
 		when().
 		    get("/demo").
 		then().
-		    statusCode(200).
-		    body("value", equalTo(0));
+		    spec(spec);
 	}
 
 	@Test
 	public void testExamplePathParam() {
+		RequestSpecification spec = new RequestSpecBuilder().addPathParam("id", 2).build(); 
+		
 		given().
 		    // .param("name", "value")
 		    // .header("TestId", "testExamplePathParam")
-		    pathParam("id", 2).
+		    // pathParam("id", 2).
+		    spec(spec).
 		when().
 		    get("/demo/{id}").
 		then().
